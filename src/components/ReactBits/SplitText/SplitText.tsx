1cca772114d5343a2c169651ae95ff3e548301d5
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-01-22 18:00:00 -0500

    Commit con fecha 22/01 a las 18:00

--- src/components/ReactBits/SplitText/SplitText.tsx
+++ src/components/ReactBits/SplitText/SplitText.tsx
@@ -64,7 +64,7 @@ const SplitText: React.FC<SplitTextProps> = ({
       if (el._rbsplitInstance) {
         try {
           el._rbsplitInstance.revert();
-        } catch (_) {}
+        } catch {}
         el._rbsplitInstance = undefined;
       }
 
@@ -128,7 +128,7 @@ const SplitText: React.FC<SplitTextProps> = ({
         });
         try {
           splitInstance.revert();
-        } catch (_) {}
+        } catch {}
         el._rbsplitInstance = undefined;
       };
     },
