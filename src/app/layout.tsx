36860788620a72693fcbf941dadc86f11cd9381a
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-02-21 23:34:51 -0500

    navbar inicial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
+import { Navbar } from "@/components/NavBar";
+import '@/sass/main.scss';
+
 export default function RootLayout({ children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
       <body>
+        <Navbar/> 
         {children}
       </body>
     </html>
