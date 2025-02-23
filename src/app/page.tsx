8bc37856e45a70a5d0178cce7221d6585445a6dc
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-02-22 20:26:02 -0500

    title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
+import { Title } from "@/components/Private/Home/Title";
+
 export default function Home() {
   return (
     <div >
-      <main>
-        <h1>Bienvenido a mi portafolio</h1>
-        <p>Soy un desarrollador especializado en React y Next.js</p>
-      </main>
+      <Title />
     </div>
   );
 }
---
+++ src/components/Private/Home/Title/Title.tsx
@@ -0,0 +1,12 @@
+import styles from './Title.module.scss';
+
+export const Title = () => {
+    return (
+        <div className={styles.Home}>
+            <main>
+                <h1>Castillo Rabanal Luis Javier</h1>
+                <h2>Full Stack Developer</h2>
+            </main>
+        </div>
+    )
+}
