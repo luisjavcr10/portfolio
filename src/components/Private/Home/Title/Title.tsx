995188f16f80ebb21d46739caad8dbec65f0b024
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-02-26 23:51:30 -0500

    mejoras en el home

--- src/components/Private/Home/Title/Title.tsx
+++ src/components/Private/Home/Title/Title.tsx
@@ -1,12 +1,28 @@
 import styles from './Title.module.scss';
+import Image from 'next/image';
 
 export const Title = () => {
-    return (
-        <div className={styles.Home}>
-            <main>
-                <h1>Castillo Rabanal Luis Javier</h1>
-                <h2>Full Stack Developer</h2>
-            </main>
+  return (
+    <div className={styles.Home}>
+      <main>
+        <div className={styles.Home__Profile}>
+          <Image
+            className={styles.Home__Profile__Image}
+            src="/images/profile/profile.jpg"
+            width={100} 
+            height={100}
+            alt="profile"
+          />
+          <button className={styles.Home__Profile__Button}>Disponible para trabajar</button>
         </div>
-    )
-}
+        <div className={styles.Home__Presentation}>
+          <h1>Hola!! Soy Luis</h1>
+          <h2>Full Stack Developer</h2>
+          <p>
+            Apasionado por la tecnología y el desarrollo web. Con experiencia en frontend y backend, creando soluciones eficientes y escalables.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+};
