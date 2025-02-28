961d0318bf9dcc5e1e3c43b3872c2e4ccaf7216b
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-02-27 21:46:34 -0500

    first home version is over

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Title } from "@/components/Private/Home/Title";
+import {ContactButtons} from "@/components/Private/Home/ContactButtons";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <div >
+    <div className={styles.Home_Container}>
       <Title />
+      <ContactButtons/>
     </div>
   );
 }
